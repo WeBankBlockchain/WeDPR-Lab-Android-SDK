ef9a4dc2b77d8be03a7e3f2d165959b3e77fdb20
HaoXuan40404 <444649358@qq.com>
2020-11-24 14:19:59 +0800

    add selective disclosure

---
+++ demo/app/src/main/java/com/webank/wedpr/selectivedisclosure/PredicateType.java
@@ -0,0 +1,11 @@
+// Copyright 2020 WeDPR Lab Project Authors. Licensed under Apache-2.0.
+
+package com.webank.wedpr.selectivedisclosure;
+
+public enum PredicateType {
+    GE,
+    LE,
+    GT,
+    LT,
+    EQ
+}
