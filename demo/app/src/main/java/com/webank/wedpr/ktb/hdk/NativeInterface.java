863e336e4ab1f41facdfa53f9e92756a577a4fe9
HaoXuan40404 <444649358@qq.com>
2021-03-25 10:27:46 +0800

    add ktb demo

---
+++ demo/app/src/main/java/com/webank/wedpr/ktb/hdk/NativeInterface.java
@@ -0,0 +1,20 @@
+// Copyright 2020 WeDPR Lab Project Authors. Licensed under Apache-2.0.
+
+package com.webank.wedpr.ktb.hdk;
+
+import java.io.IOException;
+
+/** Native interface for Crypto client. */
+public class NativeInterface {
+
+  static {
+    System.loadLibrary("ffi_java_ktb");
+  }
+
+  // JNI function section.
+  public static native HdkResult createMnemonicEn(int wordCount);
+
+  public static native HdkResult createMasterKeyEn(String password, String mnemonic);
+
+  public static native HdkResult deriveExtendedKey(String masterKey, int purposeType, int assetType, int account, int change, int addressIndex);
+}
